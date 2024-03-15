be57f402eb131c1d2259ca85c23db5c8f9393bdf
jacksonrcmachado <jacksonmachado@gmail.com>
2024-03-15 19:51:13 -0300

    Entrega do exercício P.2.12 - Acrescentando comentários

--- Exercicio_Aula01_P.2.12/menor_recursivo_modelo.ts
+++ Exercicio_Aula01_P.2.12/menor_recursivo_modelo.ts
@@ -1,13 +1,17 @@
 function menor_r(a: number[]): number {
-    if (a.length === 1) {
+    if (a.length == 1) {
+        //Se caso base atingido, retornar valor abaixo
         return a[0];
     }
+    // Dividir array ao meio e arredondar em caso de itens ímpar no array
     const meio = Math.floor(a.length / 2);
+    // Chamada recursiva
     const metade_esq = a.slice(0, meio);
-    const metade_dir = a.slice(meio);
-    const menor_esq = menor_r(metade_esq)
-    const menor_dir = menor_r(metade_dir)
-
+    const metade_dir = a.slice(meio, a.length);
+    const menor_esq = menor_r(metade_esq);
+    const menor_dir = menor_r(metade_dir);
+    
+    // Comparação final entre os arrays divididos
     if (menor_esq < menor_dir){
          return menor_esq
     }
